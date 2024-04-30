82112bc63e9c234f4b6e5cb0c4a1f6064f125fd0
lakshayks664 <lakshay.saini@ksolves.com>
2024-05-01 00:37:53 +0530

    user and video models and jwt

---
+++ src/models/user.model.js
@@ -0,0 +1,72 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const Userschema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: { type: String, required: true, trim: true, index: true },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverimage: {
+      type: String, // cloudinary url
+    },
+    watchHistory: [{ type: Schema.Types.ObjectId, ref: "Video" }],
+    password: { type: String, required: [true, "password is required"] },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// pre ek hook hai or we can call it a event . yha p hum userschema ko save krne s pehle ek function run krre hai.
+
+Userschema.pre("save", function (next) {
+  if (!this.isModified("password")) {
+    // password modify ho tabhi use encrypt krna hai
+    return next();
+  } else {
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+  }
+});
+// model k liye hum custom method bhi likh sakte hai , kuch predefined hote hai jo nahi hote unhe schme.methods m dot lga k likh do. yha old encrypted password compare krre hai.
+
+Userschema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+Userschema.methods.generateaccesstoken = async function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+Userschema.methods.generaterefreshtoken = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", Userschema);
