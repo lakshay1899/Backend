24462cb943aeebe96bebcc3df6af5510da6185f9
lakshayks664 <lakshay.saini@ksolves.com>
2024-05-04 12:31:01 +0530

    router and controllers

--- src/app.js
+++ src/app.js
@@ -18,4 +18,12 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 // to fetch data from cookies in browser
 app.use(cookieParser());
+
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+// humne yha pr app.get nahi likha because humne yha router dia hai ab jaise hi   /api/v1/users/register dalega vo yha ayega or yha s userouter pr chala jayega and then vha jo bhi function hoga use run kr lega
+
+app.use("/api/v1/users", userRouter);
+
 export default app;
